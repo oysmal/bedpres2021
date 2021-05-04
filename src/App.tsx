4f6dce1c7694f723eabeba91f7a314a04722a6f9
Øystein Malt <me@oysmal.com>
2021-05-04 21:28:31 +0200

    Clear code to be ready for workshop

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Rooms from "./pages/rooms/Rooms";
-import Estimates from "./pages/estimates/Estimates";
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact={true} path="/estimates/:id" component={Estimates} />
-        <Route exact={true} path="/" component={Rooms} />
+        <Route path="/">Welcome to the workshop!</Route>
       </Switch>
     </BrowserRouter>
   );
--- src/pages/estimates/cards/Cards.tsx
+++ src/pages/estimates/cards/Cards.tsx
@@ -1,31 +1,3 @@
 import "./Cards.css";
 
 const cards = [1, 3, 5, 8, 13, 20, 40, 60];
-
-type CardsProps = {
-  onSelectCard: (value: number) => void;
-  selectedCard: number;
-};
-
-export default function Cards(props: CardsProps) {
-  const { onSelectCard, selectedCard } = props;
-
-  return (
-    <div className="cards">
-      {cards.map((card) => {
-        let className = "card";
-        if (card === selectedCard) className += " selected-card";
-
-        return (
-          <div
-            key={card}
-            className={className}
-            onClick={() => onSelectCard(card)}
-          >
-            <h2>{card}</h2>
-          </div>
-        );
-      })}
-    </div>
-  );
-}
